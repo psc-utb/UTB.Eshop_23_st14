caaf8b7f85ab20a5dcfc1f39c33664eeecfdf2fe
Tom Vogeltanz <tom.vogeltanz@gmail.com>
2023-11-26 22:31:14 +0100

    Identity
    + Instalace Identity do Infrastructure -> Microsoft.AspNetCore.Identity.EntityFrameworkCore
    + Vytvoření entit pro Identity: User, Role
    + Vytvoření enumu pro zjednodušení práce s rolemi: Roles
    ~ Změna dědičnosti u EshopDbContext -> nyní dědí z IdentityDbContext
    + Přidání vytváření rolí, uživatelů a jejich propojení do DatabaseInit
    + Přidání inicializace do EshopDbContext s použitím DatabaseInit
    + Vytvoření LoginViewModel a RegisterViewModel pro formuláře s registrací a přihlašováním
    + Vytvoření IAccountService a AccountIdentityService pro registraci, login a logout uživatele
    + Vytvoření area Security s funkčním Controllerem Account
    + Přidání views pro areu Security -> formuláře pro registraci a přihlášení
    + Přidání konfigurace Identity a registrace služby IAccountService do Program.cs
    + Přidání migrace pro Identity
    + Přidání tlačítek pro přihlášení, registraci a odhlášení do navbaru v Layoutu
    ~ Změna zobrazení odkazu na administrátorskou stránku produktu v navbaru v Layoutu -> zobrazí se jen adminovi a managerovi
    + Přidány css souboru ikonek Bootstrapu do Layoutu
    ~ Aplikování atributu pro autorizaci na ProductController -> nyní jej může použít pouze admin a manager.

--- UTB.Eshop.Web/Program.cs
+++ UTB.Eshop.Web/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using UTB.Eshop.Application.Abstraction;
 using UTB.Eshop.Application.Implementation;
 using UTB.Eshop.Infrastructure.Database;
+using Microsoft.AspNetCore.Identity;
+using UTB.Eshop.Infrastructure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,39 @@ builder.Services.AddControllersWithViews();
 string connectionString = builder.Configuration.GetConnectionString("MySQL");
 ServerVersion serverVersion = new MySqlServerVersion("8.0.34");
 builder.Services.AddDbContext<EshopDbContext>(optionsBuilder => optionsBuilder.UseMySql(connectionString, serverVersion));
+
+
+builder.Services.AddIdentity<User, Role>()
+     .AddEntityFrameworkStores<EshopDbContext>()
+     .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 1;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequiredUniqueChars = 1;
+
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.MaxFailedAccessAttempts = 10;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+
+    options.User.RequireUniqueEmail = true;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    options.LoginPath = "/Security/Account/Login";
+    options.LogoutPath = "/Security/Account/Logout";
+    options.SlidingExpiration = true;
+});
+
+
+builder.Services.AddScoped<IAccountService, AccountIdentityService>();
 
 builder.Services.AddScoped<IFileUploadService, FileUploadService>(serviceProvider => new FileUploadService(serviceProvider.GetService<IWebHostEnvironment>().WebRootPath));
 builder.Services.AddScoped<IProductAppService, ProductAppService>();
@@ -31,6 +66,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
