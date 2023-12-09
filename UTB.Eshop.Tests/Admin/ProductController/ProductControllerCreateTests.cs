2876ff72dace3fe260efa83f6f9dcec250c89f2d
Tom Vogeltanz <tom.vogeltanz@gmail.com>
2023-12-09 19:37:28 +0100

    Testování
    + Instalován NuGet package: Moq (pro falešné závislosti) a Microsoft.EntityFrameworkCore.InMemory (pro vytvoření databáze v paměti)
    + Vytvořeny 3 testy pro přihlášení uživatele (pro AccountController) -> první s úspěšným přihlášením (unit test), druhý s přímo nastavenou chybou validace (unit test) a třetí s provedením validace současně s přihlášením (integrační test)
    + Vytvořen ObjectValidator pro umožnění validací pro integrační testy (viz poslední test v AccountControllerLoginTests).
    + Vytvořen test pro vložení nového produktu pro část ProductController -> Mock s využitím DatabaseFake.
    + Vytvořen test pro vložení nového produktu pro část ProductService -> Mock IFormFile a využití EntityFrameworkCore databáze v paměti

---
+++ UTB.Eshop.Tests/Admin/ProductController/ProductControllerCreateTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+using UTB.Eshop.Application.Abstraction;
+using UTB.Eshop.Domain.Entities;
+using UTB.Eshop.Infrastructure.Database;
+using UTB.Eshop.Web.Areas.Admin.Controllers;
+
+namespace UTB.Eshop.Tests.Admin.ProductController
+{
+    public class ProductControllerCreateTests
+    {
+        [Fact]
+        public async Task Create_success()
+        {
+            //Arrange
+            DatabaseFake.Products.Clear();
+            Mock<IProductAppService> productServiceMock = new Mock<IProductAppService>();
+            productServiceMock.Setup(productService => productService.Create(It.IsAny<Product>()))
+                                        .Returns<Product>(product => Task.Run(() => { DatabaseFake.Products.Add(product); }));
+
+            var product = GetProduct();
+
+            var productController = new Web.Areas.Admin.Controllers.ProductController(productServiceMock.Object);
+
+
+            //Act
+            var actionResult = await productController.Create(product);
+
+
+            //Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(actionResult);
+            Assert.NotNull(redirectToActionResult.ActionName);
+            Assert.Equal(nameof(Web.Areas.Admin.Controllers.ProductController.Index), redirectToActionResult.ActionName);
+
+            Assert.NotEmpty(DatabaseFake.Products);
+            Assert.Single(DatabaseFake.Products);
+        }
+
+        Product GetProduct()
+        {
+            return new Product()
+            {
+                Id = 1,
+                Name = "Produkt",
+                Price = 1.0,
+                Description = String.Empty,
+                ImageSrc = "/superimage.jpeg",
+                Image = new Mock<IFormFile>().Object
+            };
+        }
+    }
+}
+
---
+++ UTB.Eshop.Tests/ApplicationLayer/ProductServiceCreateTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+using Moq;
+
+using UTB.Eshop.Application.Abstraction;
+using UTB.Eshop.Infrastructure.Database;
+using UTB.Eshop.Domain.Entities;
+using UTB.Eshop.Application.Implementation;
+
+namespace UTB.Eshop.Tests.ApplicationLayer
+{
+    public class ProductServiceCreateTests
+    {
+        [Fact]
+        public async Task Create_Success()
+        {
+            // Arrange
+
+            //nastaveni falesne sluzby pro produkty
+            var pathToImage = "img/product/UploadImageFile.png";
+            var fileUpload = new Mock<IFileUploadService>();
+            fileUpload.Setup(fu => fu.FileUploadAsync(It.IsAny<IFormFile>(), It.IsAny<string>())).Returns(() => Task.Run<string>(() => pathToImage));
+
+
+            //nastaveni falesne IFormFile
+            Mock<IFormFile> iffMock = new Mock<IFormFile>();
+            iffMock.Setup(iff => iff.CopyToAsync(It.IsAny<Stream>(), CancellationToken.None))
+                                    .Callback<Stream, CancellationToken>((stream, token) =>
+                                    {
+                                        return;
+                                    })
+                                    .Returns(Task.CompletedTask);
+
+
+
+            //Nainstalovan Nuget package: Microsoft.EntityFrameworkCore.InMemory
+            //databazi vytvori v pameti
+            DbContextOptions options = new DbContextOptionsBuilder<EshopDbContext>()
+                                       .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                                       .Options;
+            var databaseContext = new EshopDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            //smazani inicializacnich dat, pokud existuji
+            databaseContext.Products.RemoveRange(databaseContext.Products);
+            databaseContext.SaveChanges();
+
+
+
+            ProductAppService service = new ProductAppService(fileUpload.Object, databaseContext);
+
+
+            Product testProduct = GetTestProduct(iffMock.Object);
+
+
+
+            //Act
+            await service.Create(testProduct);
+
+
+
+            // Assert
+            Assert.Single(databaseContext.Products);
+
+            Product addedProduct = databaseContext.Products.First();
+            Assert.Equal(testProduct.Id, addedProduct.Id);
+            Assert.NotNull(addedProduct.Name);
+            Assert.Matches(testProduct.Name, addedProduct.Name);
+            Assert.Equal(testProduct.Price, addedProduct.Price);
+            Assert.NotNull(addedProduct.ImageSrc);
+            Assert.Matches(pathToImage, addedProduct.ImageSrc);
+
+        }
+
+
+
+        Product GetTestProduct(IFormFile iff)
+        {
+            return new Product()
+            {
+                Id = 1,
+                Name = "produkt",
+                Price = 10,
+                ImageSrc = String.Empty,
+                Image = iff
+            };
+        }
+
+    }
+}
